189619b7df7e69f2c5a35448a743f5e55ac91129
Nikita Karapuzov <karapuzov.nikita@gmail.com>
2020-03-29 22:24:46 +0700

    useForm multiple types support

--- src/hooks/useForm/index.ts
+++ src/hooks/useForm/index.ts
@@ -1,22 +1,73 @@
-import { useReducer, Reducer } from "react";
+import { useReducer, Reducer, ChangeEvent } from "react";
 
-interface IAction {
-  name: string;
+type Event = ChangeEvent<HTMLInputElement>;
+
+interface InputValue {
   value: any;
+  validate?(value: any): any;
 }
 
-const formReducer: Reducer<any, IAction> = (state, { name, value }) => ({
-  ...state,
-  [name]: value,
-});
+interface InitialState {
+  [key: string]: InputValue;
+}
 
-const useForm = (initialState: any) => {
-  const [state, dispatch] = useReducer<Reducer<any, IAction>>(
-    formReducer,
-    initialState
+interface FormState {
+  [key: string]: any;
+}
+
+interface FormAction {
+  name: string;
+  value: any;
+  type?: string;
+  checked?: boolean;
+}
+
+function parseState(state: InitialState): FormState {
+  return Object.keys(state).reduce((acc: FormState, key: string) => {
+    acc[key] = state[key].value;
+    return acc;
+  }, {});
+}
+
+function reducer(state: FormState, action: FormAction): FormState {
+  const { name, value, type, checked } = action;
+
+  if (type === "checkbox") {
+    return {
+      ...state,
+      [name]: checked,
+    };
+  }
+
+  return {
+    ...state,
+    [name]: value,
+  };
+}
+
+function useForm(
+  initialState: InitialState
+): [FormState, (event: Event) => void] {
+  const parsedState = parseState(initialState);
+  const [state, dispatch] = useReducer<Reducer<FormState, FormAction>>(
+    reducer,
+    parsedState
   );
 
-  return [state, dispatch];
-};
+  function onChange(event: Event): void {
+    const target = event?.currentTarget || event?.target;
+    if (!target?.name) return;
+
+    const validator = initialState?.[target.name]?.validate;
+    if (validator) {
+      const validValue = validator(target.value);
+      dispatch({ ...target, value: validValue });
+    } else {
+      dispatch(target);
+    }
+  }
+
+  return [state, onChange];
+}
 
 export default useForm;
